f7ef455771290fc24003607b37b052b7e6729a0e
Ben Milton <ben.c.milton@gmail.com>
2015-05-01 15:59:38 -0400

    true initial commit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,83 +1,121 @@
-// Ionic Starter App
+angular.module('starter', ['ionic', 'starter.controllers', 'starter.services', 'ionic-timepicker'])
 
-// angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
-// the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
-
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleLightContent();
-    }
-  });
-})
-
-.config(function($stateProvider, $urlRouterProvider) {
-
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
-
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
-
-  // Each tab has its own nav history stack:
-
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
-
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
+    .run(function($ionicPlatform) {
+      $ionicPlatform.ready(function() {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        if (window.cordova && window.cordova.plugins && window.cordova.plugins.Keyboard) {
+          cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
         }
-      }
-    })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+        if (window.StatusBar) {
+          // org.apache.cordova.statusbar required
+          StatusBar.styleLightContent();
         }
-      }
+      });
     })
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
+    .config(function($stateProvider, $urlRouterProvider) {
 
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
+      // Ionic uses AngularUI Router which uses the concept of states
+      // Learn more here: https://github.com/angular-ui/ui-router
+      // Set up the various states which the app can be in.
+      // Each state's controller can be found in controllers.js
+      $stateProvider
 
-});
+        // setup an abstract state for the tabs directive
+          .state('tab', {
+            url: "/tab",
+            abstract: true,
+            templateUrl: "templates/tabs.html"
+          })
+
+        // Each tab has its own nav history stack:
+
+          .state('tab.create', {
+            url: '/create',
+            views: {
+              'tab-create': {
+                templateUrl: 'templates/tab-create.html',
+                controller: 'CreateCtrl'
+              }
+            }
+          })
+
+          .state('tab.stories', {
+            url: '/stories',
+            views: {
+              'tab-chats': {
+                templateUrl: 'templates/tab-stories.html',
+                controller: 'StoriesCtrl'
+              }
+            }
+          })
+        //.state('tab.chat-detail', {
+        //  url: '/chats/:chatId',
+        //  views: {
+        //    'tab-chats': {
+        //      templateUrl: 'templates/chat-detail.html',
+        //      controller: 'ChatDetailCtrl'
+        //    }
+        //  }
+        //})
+
+          .state('tab.account', {
+            url: '/account',
+            views: {
+              'tab-account': {
+                templateUrl: 'templates/tab-account.html',
+                controller: 'AccountCtrl'
+              }
+            }
+          });
+
+      // if none of the above states are matched, use this as the fallback
+      $urlRouterProvider.otherwise('/tab/create');
+
+    })
+
+    .directive('standardTimeMeridian', function() {
+      return {
+        restrict: 'AE',
+        replace: true,
+        scope: {
+          etime: '=etime'
+        },
+        template: "<strong>{{stime}}</strong>",
+        link: function(scope, elem, attrs) {
+
+          scope.stime = epochParser(scope.etime, 'time');
+
+          function prependZero(param) {
+            if (String(param).length < 2) {
+              return "0" + String(param);
+            }
+            return param;
+          }
+
+          function epochParser(val, opType) {
+            if (val === null) {
+              return "00:00";
+            } else {
+              var meridian = ['AM', 'PM'];
+
+              if (opType === 'time') {
+                var hours = parseInt(val / 3600);
+                var minutes = (val / 60) % 60;
+                var hoursRes = hours > 12 ? (hours - 12) : hours;
+
+                var currentMeridian = meridian[parseInt(hours / 12)];
+
+                return (prependZero(hoursRes) + ":" + prependZero(minutes) + " " + currentMeridian);
+              }
+            }
+          }
+
+          scope.$watch('etime', function(newValue, oldValue) {
+            scope.stime = epochParser(scope.etime, 'time');
+          });
+
+        }
+      };
+    });
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -1,50 +1,37 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
-  // Might use a resource here that returns a JSON array
-
-  // Some fake testing data
-  var chats = [{
-    id: 0,
-    name: 'Ben Sparrow',
-    lastText: 'You on your way?',
-    face: 'https://pbs.twimg.com/profile_images/514549811765211136/9SgAuHeY.png'
-  }, {
-    id: 1,
-    name: 'Max Lynx',
-    lastText: 'Hey, it\'s me',
-    face: 'https://avatars3.githubusercontent.com/u/11214?v=3&s=460'
-  }, {
-    id: 2,
-    name: 'Andrew Jostlin',
-    lastText: 'Did you get the ice cream?',
-    face: 'https://pbs.twimg.com/profile_images/491274378181488640/Tti0fFVJ.jpeg'
-  }, {
-    id: 3,
-    name: 'Adam Bradleyson',
-    lastText: 'I should buy a boat',
-    face: 'https://pbs.twimg.com/profile_images/479090794058379264/84TKj_qa.jpeg'
-  }, {
-    id: 4,
-    name: 'Perry Governor',
-    lastText: 'Look at my mukluks!',
-    face: 'https://pbs.twimg.com/profile_images/491995398135767040/ie2Z_V6e.jpeg'
-  }];
+.factory('Stories', function($http) {
 
   return {
-    all: function() {
-      return chats;
+    startStory: function() {
+
     },
-    remove: function(chat) {
-      chats.splice(chats.indexOf(chat), 1);
-    },
-    get: function(chatId) {
-      for (var i = 0; i < chats.length; i++) {
-        if (chats[i].id === parseInt(chatId)) {
-          return chats[i];
+
+    endStory: function() {
+      return $http.get('http://localhost:3001/end').then(function(res) {
+        console.log('endStory in factory');
+        return res.data;
+      });
+    }
+
+  };
+
+})
+
+    .factory('Camera', ['$q', function($q) {
+
+      return {
+        getPicture: function(options) {
+          var q = $q.defer();
+
+          navigator.camera.getPicture(function(result) {
+            // Do any magic you need
+            q.resolve(result);
+          }, function(err) {
+            q.reject(err);
+          }, options);
+
+          return q.promise;
         }
       }
-      return null;
-    }
-  };
-});
+    }]);
